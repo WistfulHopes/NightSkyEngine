1650423f76de8d62f75bd5b774c21ecacbf319b8
= <kp72002@gmail.com>
2023-09-16 13:33:54 -0700

    updated to latest build settings

--- Source/NightSkyEngineEditor.Target.cs
+++ Source/NightSkyEngineEditor.Target.cs
@@ -8,7 +8,7 @@ public class NightSkyEngineEditorTarget : TargetRules
 	public NightSkyEngineEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("NightSkyEngine");
 	}
