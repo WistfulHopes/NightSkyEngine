2427f891ca7c4fdc5d29bf407612daef7533fe97
= <kp72002@gmail.com>
2024-04-22 10:12:39 -0700

    docs update

--- dir_b3024b2cdfee85c01df36c10491c1f9b.js
+++ dir_b3024b2cdfee85c01df36c10491c1f9b.js
@@ -1,4 +1,4 @@
 var dir_b3024b2cdfee85c01df36c10491c1f9b =
 [
-    [ "quick-start", "dir_c1a28e943dca30e423219d4704162a1b.html", null ]
+    [ "quickstart", "dir_1c006af1a6eb59edd0a0e07422f61689.html", null ]
 ];
