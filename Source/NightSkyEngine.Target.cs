05d9efafef0e09292585833c9930e252936e0e72
WistfulHopes <9942055+WistfulHopes@users.noreply.github.com>
2025-08-07 20:53:15 -0700

    readd target.cs

---
+++ Source/NightSkyEngine.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class NightSkyEngineTarget : TargetRules
+{
+	public NightSkyEngineTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		ExtraModuleNames.Add("NightSkyEngine");
+	}
+}
---
+++ Source/NightSkyEngineEditor.Target.cs
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class NightSkyEngineEditorTarget : TargetRules
+{
+	public NightSkyEngineEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
+		ExtraModuleNames.AddRange( new string[] {
+			"NightSkyEngine",
+            "CollisionEditor",
+        });
+	}
+}
