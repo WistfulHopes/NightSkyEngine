bd8899aea58339b03e902adf1671d71a16a3048a
WistfulHopes <9942055+WistfulHopes@users.noreply.github.com>
2025-08-16 19:01:27 -0700

    fix project packaging for plugin nse

--- Plugins/NightSkyEngine/Source/NightSkyEngine/NightSkyEngine.cpp
+++ Plugins/NightSkyEngine/Source/NightSkyEngine/NightSkyEngine.cpp
@@ -3,4 +3,4 @@
 #include "NightSkyEngine.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NightSkyEngine, "NightSkyEngine" );
+IMPLEMENT_MODULE(FDefaultModuleImpl, NightSkyEngine);
