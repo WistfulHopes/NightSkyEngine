30624a2e2e5278cc687d63443fc28e5c15109bc2
lydia <lydia.d.fe@gmail.com>
2026-06-26 20:16:09 -0700

    include todo for comboable throw interaction

--- Plugins/NightSkyEngine/Source/NightSkyEngine/Battle/Objects/PlayerObject.cpp
+++ Plugins/NightSkyEngine/Source/NightSkyEngine/Battle/Objects/PlayerObject.cpp
@@ -1524,7 +1524,7 @@ void APlayerObject::ThrowEnd()
 	if (!Enemy) return;
 	Enemy->PlayerFlags &= ~PLF_IsThrowLock;
 	// Clear stun flag in case the throw did damage via a normal hitbox/hurtbox interaction.
-	// Might have implications for how tag fighters have to implement their throws for throws be comboable.
+	// TODO: investigate how this will interact with comboable throws.
 	Enemy->PlayerFlags &= ~PLF_IsStunned;
 }
 
